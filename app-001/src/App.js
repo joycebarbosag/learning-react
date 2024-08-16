5e884094813480da4127a74dea0728966569f4f3
aluno <aaaaaaa@gmail.com>
2024-08-15 21:14:17 -0300

    React - Hello World

---
+++ app-001/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+//para ser componente no react tem que ter letra maiúscula (camel case)
+function MeuComponente(){
+  return(
+    <div>
+    <h1>Olá</h1>
+    </div>
+  );
+}
+
+//a classe app funciona como a main do java
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <MeuComponente></MeuComponente>
+        <p>
+          Hello world
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
